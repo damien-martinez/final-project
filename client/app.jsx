0adeba904e7c60a23764e7a5eada11be184765bc
damien-martinez <mart.damien1@gmail.com>
2022-04-22 01:07:28 +0000

    Working on building out a homepage

--- client/app.jsx
+++ client/app.jsx
@@ -1,12 +1,40 @@
 import React from 'react';
-
+import parseRoute from './lib/parse-route';
 import WorkoutSession from './pages/workout-session';
+import Home from './pages/home';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      route: parseRoute(window.location.hash)
+    };
+  }
+
+  componentDidMount() {
+
+    window.addEventListener('hashchange', event => {
+      this.setState({ route: parseRoute(window.location.hash) });
+
+    });
+
+  }
+
+  renderPage() {
+    const { route } = this.state;
+    if (route === '') {
+      return <Home />;
+    }
+    if (route.path === 'workout-session') {
+      return <WorkoutSession/>;
+    }
+
+  }
+
   render() {
     return (
     <>
-      <WorkoutSession />
+      {this.renderPage()}
     </>
 
     );
